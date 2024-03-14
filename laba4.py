a891046d6e3de1da1d911285e4890bf482b6b704
licsz <105279457+licsz@users.noreply.github.com>
2024-03-14 13:02:50 +0400

    Update laba4.py

--- laba4.py
+++ laba4.py
@@ -1,3 +1,8 @@
+#27.	Формируется матрица F следующим образом: скопировать в нее А и  если в Е сумма чисел по периметру больше, чем количество
+# нулей по периметру , то поменять местами  В и С симметрично, иначе В и Е поменять местами несимметрично. При этом матрица А не меняется.
+# После чего если определитель матрицы А больше суммы диагональных элементов матрицы F, то вычисляется выражение: A*AT – K * F,
+# иначе вычисляется выражение (A-1 +G-F-1)*K, где G-нижняя треугольная матрица, полученная из А. Выводятся по мере формирования А, F и все
+# матричные операции последовательно.
 import numpy as np
 from matplotlib import pyplot as plt
 n = int(input("Введите размер массива "))
@@ -25,7 +30,6 @@ def print_mat(mat,discription):
     plt.title(discription)
     plt.colorbar()
     plt.show()
-
 size = len(A)//2
 if len(A)%2 == 0:
     B = A[:size, :size]
@@ -37,14 +41,12 @@ else:
     C = A[:size, size+1:]
     D = A[size+1:, :size]
     E = A[size+1:, size+1:]
-
 print_mat(A,"Матрица A:\n")
 print_mat(B,"Матрица B:\n")
 print_mat(C,"Матрица C:\n")
 print_mat(D,"Матрица D:\n")
 print_mat(E,"Матрица E:\n")
 perimeter_e = np.concatenate((E[0, :], E[1:-1, -1], E[-1, ::-1], E[1:-1, 0][::-1])).tolist()
-
 if sum(perimeter_e) > perimeter_e.count(0):
     (B, C) = (np.flip(C, axis=1), np.flip(B, axis=1))
 else:
@@ -59,5 +61,4 @@ if np.linalg.det(A) > diagonal_sum:
 else:
     result = (np.linalg.inv(A) + np.tril(A) - np.linalg.inv(F)) * k
     discription = "(A-1 +G-F-1)*K"
-
 print_mat(result, discription)
